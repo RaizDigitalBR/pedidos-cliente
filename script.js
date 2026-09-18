8bc82ff5d43dd425be04849e404ed927573588c4
leirbag4313 <gabrieljose4314@gmail.com>
2026-09-18 18:07:19 -0300

    1.6

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ if ('Notification' in window && Notification.permission === 'default') {
 
 function notificarSistema(titulo, corpo) {
     if ('Notification' in window && Notification.permission === 'granted') {
-        new Notification(titulo, { body: corpo, icon: 'imperio_logo_silver.png' });
+        new Notification(titulo, { body: corpo });
     }
 }
 
